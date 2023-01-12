cdd115819a927d66def89ccbbeff454a541ebc63
Muthuraman Chidambaram <ctmuthu93@gmail.com>
2023-01-12 13:58:39 +0100

    a simple request added

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import sys
 import requests
 
 print(sys.version)
+
+request = requests.get("https://raw.githubusercontent.com/ctmuthu/bazel_basic/master/WORKSPACE", verify=False)
+print(request.content)
